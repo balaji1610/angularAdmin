1e1951fa578f28dfe32262f5bafbda2453a3cf93
balaji1610 <balajiece1610@gmail.com>
2022-04-05 13:43:19 +0530

    Not fix code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+
+  // constructor(private router:Router){}
+
   title = 'angularAdmin';
 
  displayData: any;
+  // router: any;
+
   GetDataForm(data: NgForm) {
     this.displayData = data;
   }
+
+  // login(){
+  //   this.Router.navigate(["admin"]);
+
+  // }
 }
