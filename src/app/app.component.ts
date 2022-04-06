bcb7e2fbbb11bf0e73f5d378f097733710dd4cbd
balaji1610 <balajiece1610@gmail.com>
2022-04-06 18:24:11 +0530

    Fix event routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,12 +9,12 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
 
-  // constructor(private router:Router){}
+  constructor(private router:Router){}
 
   title = 'angularAdmin';
 
  displayData: any;
-  Router: any;
+
 
   GetDataForm(data: NgForm) {
     this.displayData = data;
@@ -28,6 +28,12 @@ export class AppComponent {
   //   this.admin = this.admin?false:true;
 
   // }
+  visible:boolean = true;
+  login() {
+      this.router.navigate(["admin"]);
+      this.visible = this.visible?false:true;
+}
+
 
 
 
