411a072b2c2391198c5c350f8ef920fa9add4c74
balaji1610 <balajiece1610@gmail.com>
2022-04-08 16:48:34 +0530

    Json Table

--- src/app/dashboard/users/users.component.ts
+++ src/app/dashboard/users/users.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Student {
+
+ id: Number;
+
+  name: String;
+
+  email: String;
+
+  gender: String;
+
+}
+
+
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -11,5 +25,70 @@ export class UsersComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  students: Student[] = [
+
+    {
+
+      id: 1,
+
+      name: "Hardik",
+
+      email: "hardik@gmail.com",
+
+      gender: "male"
+
+    },
+
+    {
+
+      id: 2,
+
+      name: "Paresh",
+
+      email: "Paresh@gmail.com",
+
+      gender: "male"
+
+    },
+
+    {
+
+      id: 3,
+
+      name: "Kiran",
+
+      email: "kiran@gmail.com",
+
+      gender: "female"
+
+    },
+
+    {
+
+      id: 4,
+
+      name: "Mahesh",
+
+      email: "mahesh@gmail.com",
+
+      gender: "male"
+
+    },
+
+    {
+
+      id: 5,
+
+      name: "Karan",
+
+      email: "karan@gmail.com",
+
+      gender: "male"
+
+    },
+
+  ]
+
 
 }
